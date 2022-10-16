5b1d17f3123d0477e83e73bcb17ae3ba2f348069
jot <hereyouare1987@gmail.com>
2022-10-16 22:16:29 +0200

    Custom pages

--- public/js/admin/custom-pages-form.js
+++ public/js/admin/custom-pages-form.js
@@ -1,10 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!*****************************************************!*\
-  !*** ./resources/js/admin/custom-pages-form/app.js ***!
-  \*****************************************************/
-$(document).ready(function () {
-  CKEDITOR.replace('content');
-});
-/******/ })()
-;
+$(document).ready((function(){CKEDITOR.replace("content")}));
