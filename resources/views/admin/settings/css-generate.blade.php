8d2b149bbafd334154082ca256bc20e3b9a81549
jot <hereyouare1987@gmail.com>
2022-11-04 18:51:34 +0100

    Admin:Settings:Main

--- resources/views/admin/settings/css-generate.blade.php
+++ resources/views/admin/settings/css-generate.blade.php
@@ -13,6 +13,8 @@ header .login-form-menu ul li a, header .login-form-menu ul li span { color: {{ 
 .subject-text-wrap > *,
 .forum-list-wrap .forum-list-body .forum-list-title,
 .forum-list-wrap .forum-list-body .forum-list-description,
+.user-list thead th.rotate span,
+.games-list-wrap ul.participant-list-wrap li input,
 .comment-list-wrap .comment-text-wrap .comment-text > * { color: {{ $data['text_main_color'] }} }
 
 .forum-list-wrap .forum-list-info .forum-list-text,
@@ -27,7 +29,14 @@ header .login-form-menu ul li a, header .login-form-menu ul li span { color: {{ 
 
 .content-table thead tr { background-color: {{ $data['table_th_bg_color'] }} }
 .content-table thead tr th { color: {{ $data['table_th_font_color'] }} }
-.content-table tbody tr { background-color: {{ $data['table_td_bg_color'] }} }
+.content-table tbody tr,
+.user-list tbody tr,
+.games-list-wrap ul.game-list li.group-games-list { background-color: {{ $data['table_td_bg_color'] }} }
 .content-table tbody tr td,
 .content-table tbody tr td a,
-.content-table tbody tr td span { background-color: {{ $data['table_td_font_color'] }} }
+.content-table tbody tr td span,
+.user-list tbody .user-list-value .user-list-score,
+.user-list tbody .user-list-value .user-list-team { background-color: {{ $data['table_td_font_color'] }} }
+
+.user-list tbody tr.user-list-heading,
+.games-list-wrap .group-name { background-color: {{ $data['table_th_bg_color'] }}; color: {{ $data['table_th_font_color'] }} }
